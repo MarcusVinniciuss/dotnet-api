9a79986d07fff0f6ef48e93e0635114c35a18a04
AndreHBSilva <andrehugo.bs@gmail.com>
2022-10-13 21:43:06 -0300

    feat: endpoint keep alive

---
+++ EuPagoAPI/Controllers/KeepAliveController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EuPagoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KeepAliveController : ControllerBase
+    {
+
+        // GET api/<KeepAliveController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Keep Alive!";
+        }
+
+    }
+}
